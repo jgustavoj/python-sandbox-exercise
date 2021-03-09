6c20ebcc918217391b43aa46ccff55355501cad1
jgustavoj <gjime011@fiu.edu>
2021-03-09 17:06:19 -0500

    files

--- files.py
+++ files.py
@@ -1,1 +1,27 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+
+# Open a file 
+
+myFile = open('myFile.txt', 'w')
+
+# get some info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode) 
+
+# write to file
+myFile.write('I love python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# append to file
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like PHP')
+myFile.close()
+
+
+# read from file 
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(10)
+print(text)
